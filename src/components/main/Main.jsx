89ecde913f06475cd294fa4b688bdf50695d570b
yildizaydnn <yildizaydd@gmail.com>
2025-10-28 00:30:00 +0300

    first commit

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+function Main() {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
+
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, User!</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className='cards'>
+
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+
+                    </div>
+                    <div className='card'>
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+
+                    </div>
+                    <div className='card'>
+                        <p>Brainstorm team bonding activites for our work retreat.</p>
+                        <img src={assets.message_icon} alt="" />
+
+                    </div>
+                    <div className='card'>
+                        <p>Improve the readability of the following code:</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                    </div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon} alt="" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
