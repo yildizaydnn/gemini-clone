89b691443ec8dbed92f0828e204dfa81562de0c3
yildizaydnn <yildizaydd@gmail.com>
2025-10-29 18:44:06 +0300

    create ui of google gemini app

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -41,12 +41,16 @@ function Main() {
                 <div className="main-bottom">
                     <div className="search-box">
                         <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
                     </div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+                    <p className="bottom-info">
+                        Gemini may display inaccurate information.
+                    </p>
                 </div>
-
             </div>
         </div>
     )
